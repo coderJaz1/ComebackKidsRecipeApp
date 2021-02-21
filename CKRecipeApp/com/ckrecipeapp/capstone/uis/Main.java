6c132a706ab687485d821544357d36ff8f9bc48a
coderJaz <jpere44@uis.edu>
2021-02-20 20:04:51 -0700

    Committing test as "Hello World"

---
+++ CKRecipeApp/com/ckrecipeapp/capstone/uis/Main.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.ckrecipeapp.capstone.uis;
+
+/**
+ * @author jaz79
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello World");
+	}
+
+}
